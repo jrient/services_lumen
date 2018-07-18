813b84ed7cfd08fea85b28842823434691060140
jrient <jrient@qq.com>
2018-07-19 00:26:49 +0800

    start a simple calendar

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *  功能: 万年历 提供节假日查询，日历查询等功能
+ */
+
+namespace App\Http\Controllers;
+
+
+class CalendarController extends Controller
+{
+
+    /**
+     *  查询单个日期信息
+     */
+    public function date() {
+
+    }
+}
